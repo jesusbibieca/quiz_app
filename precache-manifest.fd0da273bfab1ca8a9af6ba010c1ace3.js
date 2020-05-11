2789098a112ea15e7ebb31edcd9dde55bedec285
Jesus Bibieca <jesusbibieca52@gmail.com>
2020-05-10 20:31:12 -0400

    Updates

---
+++ precache-manifest.fd0da273bfab1ca8a9af6ba010c1ace3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c79a4341e1488dedd979dbbd484694",
+    "url": "/quiz_app/index.html"
+  },
+  {
+    "revision": "6998d1bc7d0c673bf2f5",
+    "url": "/quiz_app/static/js/2.ec99dd55.chunk.js"
+  },
+  {
+    "revision": "3376583730ca32947c68cf0fdbcbb344",
+    "url": "/quiz_app/static/js/2.ec99dd55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cbaa16cabf73e0c9bf3",
+    "url": "/quiz_app/static/js/main.ea4c82c8.chunk.js"
+  },
+  {
+    "revision": "612dde8271b66882bc14",
+    "url": "/quiz_app/static/js/runtime-main.a21d1c80.js"
+  }
+]);
